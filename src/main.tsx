622e24e002a5f6d8c7c46bf111a39a0bd2454d92
milky3798 <3671337904@qq.com>
2026-03-12 21:19:15 +0800

    Implement URL redirection based on query parameter
    Add URL parameter handling for redirection.

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("p");
+
+if (redirect) {
+  window.history.replaceState({}, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
